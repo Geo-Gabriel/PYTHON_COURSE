6b26518e39549807fd1f5d5db26c874128ccd4af
G. Gabriel <geoquimics@gmail.com>
2019-11-13 11:07:31 -0300

    aula06

--- class06/aula06_FOR.py
+++ class06/aula06_FOR.py
@@ -1,7 +1,27 @@
 # TRABALHADO COM LAÇO DE REPETIÇÃO FOR 
 
-s = 0
 
-for x in range (0,40):
-    x = x + s
-    print(x)
+# FOR SIMPLES USANDO RANGE NO INTERVALO DE 0 A 10 COM INCREMENTO DE UM
+# for i in range (0,10):
+#     print(f'{i} - LOVE U')
+
+# FOR SIMPLES UTILIZANDO RANGE NO INTERVALO DE 0 A 10 COM INCREMENTO DE 2
+
+# for a in range (0,10,2):
+#     print(f'{a} - TESTE')
+
+
+# O MELHOR A SE UTILIAR EM UMA LISTA NÁO É O FOR COMO RANGE E SIM O FOR IT 
+# USA O FOR COMO RANGE QUANDO QUER IMPRIMIR UMA QUANTIDADE ESPECIFICA DE VEZES
+
+lista = ['george', 'gabriel', 'pereira', 'da']
+
+# for nomes in lista:
+#     print(nomes)
+
+lista.append('silva')
+
+n = int(input('Digite n:'))
+
+for i in range (0,11):
+    print(f'{i} x {n} = {i*n}')
