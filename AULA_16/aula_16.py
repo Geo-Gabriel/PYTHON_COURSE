c0a1230ec64d3c84400e38b50a535c984ddb701f
George Gabriel <geoquimics@gmail.com>
2020-01-17 20:34:31 -0300

    Aula 16 >> WEB Flask

--- AULA_16/aula_16.py
+++ AULA_16/aula_16.py
@@ -1,15 +1,11 @@
-# aula 16 ----- 29-11-2019
-#??
+# Aula 16 ----- 29-11-2019
 
+# Importa os métodos que estão em outro arquivo
 
-# importa os métodos que estão em outro arquivo
 from faixa import criar_faixa, salvar_faixa, ler_faixas
 
-
-
-
-#cadastro de playlist
-#lendo musica, artista, e album
+# Cadastro de playlist
+# Lendo musica, artista, e album
 
 # Inputs p/ inserir os dados da faixa
 
@@ -17,11 +13,13 @@ musica = input('Digite uma musica: ')
 artista = input('Digite artista: ')
 album = input('Digite album: ')
 
-# Aqui está chamando os métodos 
+# Aqui está chamando os métodos que estão no arquivo AULA_16/metodos_faixa.py 
 
 faixa = criar_faixa(musica, artista, album)
 salvar_faixa(faixa)
 lista = ler_faixas()
+
+# Percorre todos os elementos na lista com o laço FOR e imprime
 
 for f in lista: # 'f' se refere as faixas contidas na lista p/ faixas na lista
     print(f"{f['musica']} - {f['artista']} - {f['album']}")
