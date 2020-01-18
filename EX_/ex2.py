8182d326a17e319f926e07b8fbc7921880a16746
George Gabriel <geoquimics@gmail.com>
2020-01-17 21:30:05 -0300

    Excluindo Pasta

--- EX_/ex2.py
+++
@@ -1,6 +0,0 @@
-#Complete o código abaixo de modo que o resultado impresso seja igual ao número inteiro 36
-numero_1 = 6
- 
-numero_1 = numero_1**2
- 
-print(numero_1)
--- EX_/ex3.py
+++
@@ -1,9 +0,0 @@
-#Complete o código abaixo com o operador correto para que o resultado seja "Acesso permitido".
-
-papel_operador = "OPERADOR"
-papel_coordenador = "COORDENADOR"
- 
-papel = papel_operador
- 
-if(papel _____ papel_operador):
-    print("Acesso permitido")
